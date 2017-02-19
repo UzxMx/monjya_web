f41b6f0798cc5a9161fc5a46df470adc679269e6
Mingxiang Xue <xmx@MingxiangdeMacBook-Pro.local>
2017-02-19 12:35:34 +0800

    fix qiniu

--- config/initializers/qiniu_sdk.rb
+++ config/initializers/qiniu_sdk.rb
@@ -3,4 +3,4 @@ logger.formatter = Logger::Formatter.new
 logger.datetime_format = "%Y-%m-%d %H:%M:%S"
 Qiniu::Log.logger = logger
 Qiniu.establish_connection! access_key: '6jKARJCUgfq2gjaDzOAC8oYe6h92G_iy4BvmREaZ', secret_key: '4Wv5Rp6PLsKVHE2IDMMc4wNfHV0NQ_zhH3fmGNZ3'
-QINIU_CALLBACK_URL = Rails.env.production? ? 'http://xinya.me/api/qiniu/callback' : 'http://115.29.160.95:9000/api/qiniu/callback'
+QINIU_CALLBACK_URL = 'http://121.42.248.212/api/qiniu/callback'
